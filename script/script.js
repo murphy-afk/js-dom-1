4b34d72046bda58c2fa541b66b8cdc6914f011ff
murphy-afk <bardarenoemi@gmail.com>
2025-11-05 14:10:10 +0100

    feat: lamp on and off

--- script/script.js
+++ script/script.js
@@ -1,10 +1,15 @@
 const imgElement = document.querySelector("img");
 const btnElement = document.querySelector("button");
 
-imgElement.src = "../img/white_lamp.png";
-
-btnElement.innerText = "Accendi";
-
-btnElement.addEventListener("click", function() {
-  imgElement.src = "../img/yellow_lamp.png";
+let lampOff = true;
+btnElement.addEventListener("click", function () {
+  if (lampOff) {
+    imgElement.src = "../img/yellow_lamp.png";
+    btnElement.innerText = "Spegni";
+  }
+  else {
+    imgElement.src = "../img/white_lamp.png";
+    btnElement.innerText = "Accendi";
+  }
+  lampOff = !lampOff
 });
