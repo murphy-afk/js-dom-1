83693c60eb24279b32596099c676b3c3ae838fb4
murphy-afk <bardarenoemi@gmail.com>
2025-11-05 15:29:11 +0100

    feat: background decoration

--- script/script.js
+++ script/script.js
@@ -1,21 +1,26 @@
 const imgElement = document.querySelector("img");
 const btnElement = document.querySelector("button");
-const ovalElem = document.querySelector(".oval")
+const ovalElem = document.querySelector(".oval");
+const bodyElem = document.querySelector("body");
 
 let lampOff = true;
 btnElement.addEventListener("click", function () {
   if (lampOff) {
     imgElement.src = "../img/yellow_lamp_copy.png";
     btnElement.innerText = "Spegni";
-    btnElement.classList.add("bg-dark", "text-light");
+    btnElement.classList.add("bg-dark", "text-light", "border", "border-light");
     ovalElem.classList.add("light-on");
+    bodyElem.classList.remove("bg-dark");
+    bodyElem.classList.add("background-on");
   }
   else {
     imgElement.src = "../img/white_lamp_copy.png";
     btnElement.innerText = "Accendi";
-    btnElement.classList.remove("bg-dark", "text-light");
-    btnElement.classList.add("bg-warning");
-    ovalElem.classList.remove("light-on")
+    btnElement.classList.remove("bg-dark", "text-light", "border-light", "border");
+    btnElement.classList.add("bg-warning",);
+    ovalElem.classList.remove("light-on");
+    bodyElem.classList.remove("background-on");
+    bodyElem.classList.add("bg-dark");
   }
   lampOff = !lampOff;
 });
