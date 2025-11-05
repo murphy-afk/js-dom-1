36647bde187bd17dce1244c5f20874638fe59746
murphy-afk <bardarenoemi@gmail.com>
2025-11-05 15:58:02 +0100

    feat: added ghost

--- script/script.js
+++ script/script.js
@@ -2,10 +2,16 @@ const imgElement = document.querySelector("img");
 const btnElement = document.querySelector("button");
 const ovalElem = document.querySelector(".oval");
 const bodyElem = document.querySelector("body");
+const booElem = document.querySelector(".boo");
 
+let ghost = false;
 let lampOff = true;
 btnElement.addEventListener("click", function () {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNum);
+  // if randomNum is 3 make boo appear
   if (lampOff) {
+    if (ghost) booElem.classList.add("d-none");
     imgElement.src = "../img/yellow_lamp_copy.png";
     btnElement.innerText = "Spegni";
     btnElement.classList.add("bg-dark", "text-light", "border", "border-light");
@@ -21,6 +27,10 @@ btnElement.addEventListener("click", function () {
     ovalElem.classList.remove("light-on");
     bodyElem.classList.remove("background-on");
     bodyElem.classList.add("bg-dark");
+    if (randomNum === 3)  {
+      booElem.classList.remove("d-none");
+    ghost = true;
+    }
   }
   lampOff = !lampOff;
 });
