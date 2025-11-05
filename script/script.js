c5433c19d70af5fa3e3dc036d03aaf73f5599995
murphy-afk <bardarenoemi@gmail.com>
2025-11-05 14:51:15 +0100

    feat: image adjustment and background base

--- script/script.js
+++ script/script.js
@@ -1,15 +1,21 @@
 const imgElement = document.querySelector("img");
 const btnElement = document.querySelector("button");
+const ovalElem = document.querySelector(".oval")
 
 let lampOff = true;
 btnElement.addEventListener("click", function () {
   if (lampOff) {
-    imgElement.src = "../img/yellow_lamp.png";
+    imgElement.src = "../img/yellow_lamp_copy.png";
     btnElement.innerText = "Spegni";
+    btnElement.classList.add("bg-dark", "text-light");
+    ovalElem.classList.add("light-on");
   }
   else {
-    imgElement.src = "../img/white_lamp.png";
+    imgElement.src = "../img/white_lamp_copy.png";
     btnElement.innerText = "Accendi";
+    btnElement.classList.remove("bg-dark", "text-light");
+    btnElement.classList.add("bg-warning");
+    ovalElem.classList.remove("light-on")
   }
-  lampOff = !lampOff
+  lampOff = !lampOff;
 });
